9e2419cd099596a03759fd634098bf758a1a6fb8
Shamim Ahemad <111426896+Shamimahemad@users.noreply.github.com>
2026-05-30 05:45:04 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+
+const accountId= 786
+let accountEmail= "shamim337@gmail.com"
+var accountPassword = "786" //var ki jagah let use karenge kyunki block scope problem aat hai
+accountCity = "Gonda"
+//accountId =2
+accountEmail = "shiva@gmail"
+accountPassword = "644"
+accountCity = "Noida"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
