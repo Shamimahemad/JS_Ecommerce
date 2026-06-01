79beb59240ce5cf3d3816662881b9934700e3839
Shamim Ahemad <111426896+Shamimahemad@users.noreply.github.com>
2026-06-01 04:52:54 +0000

    converion of datatupes

---
+++ 01_basics/03_c0nversionOperation.js
@@ -0,0 +1,19 @@
+let score = "33dgf"
+//const {score} = requestAnimationFrame.apply
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33hdgf" => NaN
+// true => 1
+//false=> 0
+
+let isLogedIn = true
+let booleanIslogedIn = Boolean(isLogedIn)
+console.log(booleanIslogedIn);
+
