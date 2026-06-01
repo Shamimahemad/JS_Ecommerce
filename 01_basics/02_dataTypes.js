e25e991345110d557cfd7014925e858f1ce83c0b
Shamim Ahemad <111426896+Shamimahemad@users.noreply.github.com>
2026-06-01 04:37:17 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+"use strict";// treat all js code as newer version
+// alert (3+4)// we are using nodejs,not browser
+let name = "shaimim"
+let age = 24
+let isLogedIn = false
+console.log(typeof age);
+
+
